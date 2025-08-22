f433c58fdbda1f69652a92506c971f9155238a7c
sarasantosdev <contatosaracsantos@gmail.com>
2025-08-21 22:36:57 -0300

    Aprimora a lógica de validação de dados

--- index.js
+++ index.js
@@ -2,18 +2,44 @@ const email = document.getElementById('in-email');
 const date = document.getElementById('in-date');
 const password = document.getElementById('in-password');
 const form = document.getElementById('forms');
+const message = document.getElementById('message');
 
-form.addEventListener('submit', function(event){
+let fields = [
+    {
+        input: email,
+        field: "email",
+        regex: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i,
+        error: "Email incorreto"
+    },
+    {
+        input: date,
+        field: "date",
+        regex: /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/,
+        error: "Formato de data inválido"
+    },
+    {
+        input: password,
+        field: "password",
+        regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/,
+        error: "Senha incorreta"
+    }
+];
+
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     validar();
 });
 
 function validar() {
-    let remail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-    let redate = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
-    let repassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/;
-
-    console.log(remail.test(email.value) ? "email correto": "email incorreto");
-    console.log(redate.test(date.value) ? "data correta": "data incorreta");
-    console.log(repassword.test(password.value) ? "senha correta": "senha incorreta");
+    for (let el of fields) {
+        if (!el.input.value) {
+            message.textContent = "Preencha os campos";
+            return;
+        }
+        if (!el.regex.test(el.input.value)) {
+           message.textContent = el.error;
+           return;
+        }
+    }
+    message.textContent = "Campos preenchidos com sucesso!";
 }
